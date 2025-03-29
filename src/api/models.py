5066e6704ca99ce377324c8a439b41575835b4ed
Karoll Guzmán <karolldguzman@gmail.com>
2025-03-29 11:21:18 +0000

    modificando mi código, añadiendo el modeling y la foto del diagrama

--- src/api/models.py
+++ src/api/models.py
@@ -1,20 +1,62 @@
-from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import create_engine
+from eralchemy2 import render_er
 
+Base = declarative_base()
 
-db = SQLAlchemy()
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False, unique=True)
+    email = Column(String(50), nullable=False, unique=True)
+    password = Column(String(50), nullable=False, unique=False)
+    favorites = relationship("Favorite", back_populates="user")
 
+class Character(Base):
+    __tablename__ = 'character'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False, unique=True)
+    description = Column(String(500), nullable=False)
+    favorites = relationship("Favorite", back_populates="character")
 
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class Planet(Base):
+    __tablename__ = 'planet'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String(150), unique=True, nullable=False)
+    description = Column(String(500), nullable=False)
+    favorites = relationship("Favorite", back_populates="planet")
 
-    def __repr__(self):
-        return f'<User {self.email}>'
+class Favorite(Base):
+    __tablename__ = 'favorites'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    user = relationship("User", back_populates="favorites")
+    character = relationship("Character", back_populates="favorites")
+    planet = relationship("Planet", back_populates="favorites")
 
-    def serialize(self):
-        # Do not serialize the password, its a security breach
-        return {"id": self.id,
-            "email": self.email}
-    
+def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "character_id": self.character_id,
+            "planet_id": self.planet_id,
+            "user": self.user.name if self.user else None,  # Incluye el nombre del usuario si existe
+            "character": self.character.name if self.character else None,  # Incluye el nombre del personaje si existe
+            "planet": self.planet.planet_name if self.planet else None  # Incluye el nombre del planeta si existe
+        }
+
+## Draw from SQLAlchemy base
+render_er(Base, 'diagram.png')
